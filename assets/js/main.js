ea5b95cec30bda95e2a1c1c95d72db279ecaabec
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-29 22:53:24 +0100

    Created Ajax call to github and return function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // console.log('We are Ready')
-    $('#userSearch').on('keyup', function(){
+    $('#userSearch').on('keyup', function (e) {
         var username = e.target.value;
 
-    // Make API request to Github
-    $.ajax({
-        url:'https://api.github.com/users/' +username,
-        data:{
-            client_id: clientid,
-            client_secret: clientsecret
-        }
+        // Make API request to Github
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            data: {
+                client_id: clientid,
+                client_secret: clientsecret
+            }
+        }).done(function(userData) {
+            $('#profile').html(`
+            ${userData.name}
+            `)
+        });
     });
-
-    });
-
 });
