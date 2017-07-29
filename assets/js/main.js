2e1ca23a2c32651f7e541a56b5fb040138aede0e
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-29 23:03:57 +0100

    added avatar styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,21 @@ $(document).ready(function () {
             }
         }).done(function(userData) {
             $('#profile').html(`
-            ${userData.name}
+            <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">${userData.name}</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+              <div class="col-md-3">
+              <img src="${userData.avatar_url}" class="thumbnail gitavatar">
+              </div>
+              <div class="col-md-9">
+              
+              </div>
+              </div>
+            </div>
+          </div>
             `)
         });
     });
