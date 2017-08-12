fb5063c2c1e17d4e004bb86ad23cbdb09ab13921
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-12 11:56:19 +0100

    Fixed the infinite loop in repos. This should now be working as intended

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,74 @@
-$(document).ready(function () {
-
-    // console.log('We are Ready')
-    $('#userSearch').on('keyup', function (e) {
-        var username = e.target.value;
-
-        // Make API request to Github
+$(document).ready(function(){
+    $('#searchUser').on('keyup', function(e){
+      let username = e.target.value;
+  
+      // Make request to Github
+      $.ajax({
+        url:'https://api.github.com/users/'+username,
+        data:{
+          client_id:clientid,
+          client_secret:clientsecret
+        }
+      }).done(function(user){
         $.ajax({
-            url:'https://api.github.com/users/'+username,
-            data: {
-                client_id: clientid,
-                client_secret: clientsecret,
-                sort: 'created: asc',
-                per_page: 5
-            }
-        }).done(function(userData) {
-            $.ajax({
-                url:'https://api.github.com/users/'+username+'/repos',
-            }).done(function(repoData){
-                $.each(repoData, function(index, repoData){
-                    $('#repos').append(`
-                    <div class="well">
-                        <div class="row">
-                            <div class="col-md-7">
-                            <strong>${repoData.name}</strong> is written in mostly: <strong>${repoData.language}</strong>
-                            </div>
-                            <div class="col-md-3">
-                            <span class="label label-default">Forks: ${repoData.forks_count}</span>
-                            <span class="label label-primary">Watchers: ${repoData.watchers_count}</span>
-                            <span class="label label-success">Stars: ${repoData.stargazers_count}</span>
-                                
-                            </div>
-                            <div class="col-md-2">
-                                <a href="${repoData.html_url}" class="btn btn-primary" target="_blank">Visit Repo</a>
-                            </div>
-                        </div>
-                    </div>
-                    `);
-                });
-            });
-            $('#profile').html(`
-            <div class="panel panel-default">
+          url:'https://api.github.com/users/'+username+'/repos',
+          data:{
+            client_id:clientid,
+            client_secret:clientsecret,
+            sort: 'created: asc',
+            per_page: 5
+          }
+        }).done(function(repos){
+          $.each(repos, function(index, repo){
+            $('#repos').append(`
+              <div class="well">
+                <div class="row">
+                  <div class="col-md-7">
+                    <strong>${repo.name}</strong>: ${repo.description}
+                  </div>
+                  <div class="col-md-3">
+                    <span class="label label-default">Forks: ${repo.forks_count}</span>
+                    <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+                    <span class="label label-success">Stars: ${repo.stargazers_count}</span>
+                  </div>
+                  <div class="col-md-2">
+                    <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+                  </div>
+                </div>
+              </div>
+            `);
+          });
+        });
+        $('#profile').html(`
+          <div class="panel panel-default">
             <div class="panel-heading">
-              <h3 class="text-center">Github Profile of <strong>${userData.name}</strong></h3>
+              <h3 class="panel-title">${user.name}</h3>
             </div>
             <div class="panel-body">
               <div class="row">
-              <div class="col-md-3">
-              <img src="${userData.avatar_url}" class="gitavatar img-responsive">
-              <a href="${userData.html_url}" class="btn btn-default btn-block" target="_blank"><i class="fa fa-github"></i> Visit Profile</a>
-              </div>
-              <div class="col-md-9">
-              <span class="label label-default">Public Repos: ${userData.public_repos}</span>
-              <span class="label label-default">Public Gists: ${userData.public_gists}</span>
-              <span class="label label-info">Followers: ${userData.followers}</span>
-              <span class="label label-warning">Following: ${userData.following}</span>
-              <br><br>
-              <ul class="list-group">
-              <li class="list-group-item">Company: ${userData.company || 'No Info'}</li>
-              <li class="list-group-item">Location: ${userData.location || ''}</li>
-              <li class="list-group-item">Website: <a href="${userData.blog || ''}" target="_blank" class="label label-primary">Visit Website</a></li>
-              <li class="list-group-item">Member Since: ${userData.created_at}</li>
-              </ul>
-              </div>
+                <div class="col-md-3">
+                  <img class="gitavatar img-responsive" src="${user.avatar_url}">
+                  <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                </div>
+                <div class="col-md-9">
+                <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                <span class="label label-success">Followers: ${user.followers}</span>
+                <span class="label label-info">Following: ${user.following}</span>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item">Company: ${user.company || 'No Info'}</li>
+                  <li class="list-group-item">Location: ${user.location || ''}</li>
+                  <li class="list-group-item">Website: <a href="${user.blog || ''}" target="_blank" class="label label-primary">Visit Website</a></li>
+                  <li class="list-group-item">Member Since: (${user.created_at}</li>
+                </ul>
+                </div>
               </div>
             </div>
           </div>
           <h3 class="page-header">Latest Repos</h3>
           <div id="repos"></div>
-            `);
-        });
+        `);
+      });
     });
-});
+  });
